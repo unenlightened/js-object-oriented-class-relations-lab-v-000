8bf53d29c860a0573768b65248fe0b8513ac7011
Chely Ho <adept_inept@yahoo.com>
2018-07-04 21:21:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ class Passenger {
     this.name = name
     store.passengers.push(this)
   }
+
+  trips() {
+    return store.trips.filter(trip => trip.passengerId === this.id)
+  }
 }
 
 class Trip {
